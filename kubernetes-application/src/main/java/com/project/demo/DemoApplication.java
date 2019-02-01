f64bf4a380a2b7f0d4119b52c30b18dd315a88cf
hiennguyen@bookygroup.fi <hienminhnguyen711@gmail.com>
2019-02-01 14:31:47 +0200

    Prepare to kubernetes

--- kubernetes-application/src/main/java/com/project/demo/DemoApplication.java
+++ kubernetes-application/src/main/java/com/project/demo/DemoApplication.java
@@ -2,13 +2,30 @@ package com.project.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+
+	@GetMapping("")
+	public String helloWorld() throws UnknownHostException {
+		return "Hello from " + InetAddress.getLocalHost().getHostName() + " ... deploy the application with Kubernetes ";
+	}
+
 
 }
 
